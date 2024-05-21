ebaee145e916b36e84f7923d887317103719dfee
reynaldicheok <44896387+reynaldicheok@users.noreply.github.com>
2024-05-21 09:55:50 +0800

    Update is_valid_password.py
    Small change to min character, swapped back to 8

--- src/userapi/utils/is_valid_password.py
+++ src/userapi/utils/is_valid_password.py
@@ -1,5 +1,5 @@
 import re
 def is_valid_password(password):
-    reg = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{12,20}$"
+    reg = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,20}$"
     reg_com = re.compile(reg)
     return re.search(reg_com,password) is not None

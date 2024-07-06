13ffb3ec47f81a3b39c448a33de5ba19c49b12e2
WQYeo <yeo_dev@protonmail.com>
2024-07-06 21:08:29 +0800

    allow special characters in password regex

--- src/userapi/utils/is_valid_password.py
+++ src/userapi/utils/is_valid_password.py
@@ -1,5 +1,5 @@
 import re
 def is_valid_password(password):
-    reg = "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d_\-\!@#$%^&*]{8,72}$"
+    reg = r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*()-_=+`~[\]{}\\|;:'\",.<>/?]{8,72}$"
     reg_com = re.compile(reg)
     return re.search(reg_com,password) is not None
